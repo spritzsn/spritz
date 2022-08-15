9e2f39156ac876ea2ca5b4687c2aa437efdc848a
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-15 08:34:16 -0400

    port

--- src/main/scala/io/github/spritzsn/spritz/Main.scala
+++ src/main/scala/io/github/spritzsn/spritz/Main.scala
@@ -1,6 +1,6 @@
 package io.github.spritzsn.spritz
 
-import io.github.spritzsn.async._
+import io.github.spritzsn.async.loop
 import cps.*
 import cps.monads.FutureAsyncMonad
 import scala.concurrent.duration.*
@@ -11,6 +11,8 @@ import scala.concurrent.duration.*
     app.listen(3000)
     println("listening")
   }
+
+  loop.run()
 
 //  async {
 //    for i <- 1 to 3 do
