016e7c29b4a339a087e52347cdcf9238bf9ef30a
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-15 10:10:26 -0400

    test main

--- src/main/scala/io/github/spritzsn/spritz/Main.scala
+++ src/main/scala/io/github/spritzsn/spritz/Main.scala
@@ -1,16 +1,17 @@
-package io.github.spritzsn.spritz
+//package io.github.spritzsn.spritz
+//
+//import cps.*
+//import cps.monads.FutureAsyncMonad
+//import scala.concurrent.duration.*
+//import io.github.spritzsn.async._
+//
+//@main def run(): Unit =
 
-import cps.*
-import cps.monads.FutureAsyncMonad
-import scala.concurrent.duration.*
-import io.github.spritzsn.async._
-
-@main def run(): Unit =
-  Server { app =>
-    app.get("/", (req, res) => res.send("hello world"))
-    app.listen(3000)
-    println("listening")
-  }
+//  Server { app =>
+//    app.get("/", (req, res) => res.send("hello world"))
+//    app.listen(3000)
+//    println("listening")
+//  }
 
 //  async {
 //    for i <- 1 to 3 do
