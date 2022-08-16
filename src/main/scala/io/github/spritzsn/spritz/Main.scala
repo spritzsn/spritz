0365d7c33a8cebdc33005731799c663780896de2
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-16 17:00:02 -0400

    handle HEAD method correctly

--- src/main/scala/io/github/spritzsn/spritz/Main.scala
+++ src/main/scala/io/github/spritzsn/spritz/Main.scala
@@ -1,16 +1,16 @@
-package io.github.spritzsn.spritz
-
-import cps.*
-import cps.monads.FutureAsyncMonad
-import scala.concurrent.duration.*
-import io.github.spritzsn.async._
-
-@main def run(): Unit =
-  Server { app =>
-    app.get("/", (req, res) => res.send("hello world"))
-    app.listen(3000, "TestServer/1.0")
-    println("listening")
-  }
+//package io.github.spritzsn.spritz
+//
+//import cps.*
+//import cps.monads.FutureAsyncMonad
+//import scala.concurrent.duration.*
+//import io.github.spritzsn.async._
+//
+//@main def run(): Unit =
+//  Server { app =>
+//    app.get("/", (req, res) => res.send("hello world"))
+//    app.listen(3000, "TestServer/1.0")
+//    println("listening")
+//  }
 
 //  async {
 //    for i <- 1 to 3 do
