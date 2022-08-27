59476d0460fb6bb7bffac9395c4c04ec399f37e6
Edward A Maxedon Sr <edadma@gmail.com>
2022-08-26 20:42:09 -0400

    rewrite router

--- src/main/scala/io/github/spritzsn/spritz/responseTime.scala
+++ src/main/scala/io/github/spritzsn/spritz/responseTime.scala
@@ -3,7 +3,7 @@ package io.github.spritzsn.spritz
 import io.github.spritzsn.libuv.hrTime
 
 def responseTime(digits: Int = 3, suffix: Boolean = true): RequestHandler =
-  (req: Request, res: Response) =>
+  (req, res) =>
     val start = hrTime
 
     res action { res.set("X-Response-Time", responseTime(start, digits, suffix)) }
