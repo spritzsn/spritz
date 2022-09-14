3ce2a226fa7a05ac569a9681589acce79839d673
Edward A Maxedon Sr <edadma@gmail.com>
2022-09-14 09:19:59 -0400

    test main

--- src/main/scala/io/github/spritzsn/spritz/Main.scala
+++ src/main/scala/io/github/spritzsn/spritz/Main.scala
@@ -1,19 +1,11 @@
-package io.github.spritzsn.spritz
-
-import cps.*
-import cps.monads.FutureAsyncMonad
-
-@main def run(): Unit =
-  Server("TestServer/1") { app =>
-    app
-      .use(responseTime())
-      .use("/project", (req, res) => res.send("ok"))
-      .get(
-        "/",
-        (req, res) => async { println(req); HandlerResult.Next },
-        (req, res) => async { res.json(req.query, 2) },
-      )
-      .get("/a", (req, res) => res.send("a route"))
-    app.listen(3000)
-    println("listening")
-  }
+//package io.github.spritzsn.spritz
+//
+//import cps.*
+//import cps.monads.FutureAsyncMonad
+//
+//@main def run(): Unit =
+//  Server("TestServer/1") { app =>
+//    app.get("/", (req, res) => res.send("hello"))
+//    app.listen(3000)
+//    println("listening")
+//  }
