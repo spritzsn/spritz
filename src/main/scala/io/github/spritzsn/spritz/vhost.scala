7bd863c2c040f9b3d5e885512c368d7de762684a
Edward A Maxedon Sr <edadma@gmail.com>
2022-09-20 09:33:53 -0400

    add vhost middleware

---
+++ src/main/scala/io/github/spritzsn/spritz/vhost.scala
@@ -0,0 +1,17 @@
+package io.github.spritzsn.spritz
+
+def vhost(hostname: String, handler: RequestHandler): RequestHandler =
+  var end = 0
+  val buf = new StringBuilder
+
+  "\\*".r
+    .findAllMatchIn(hostname)
+    .foreach { m =>
+      buf ++= (if end < m.start then s"\\Q${s.substring(end, m.start)}\\E.+" else ".+")
+      end = m.end
+    }
+  if end < hostname.length then buf ++= s"\\Q${s.substring(end)}\\E"
+
+  val regex = buf.toString.r
+
+  (req, res) => if regex.matches(req.hostname) then handler(req, res) else HandlerResult.Next
